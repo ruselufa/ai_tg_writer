cf49d3eb3e4962d0c0c2b14a1881e07536761ae3
Ruslan Galiev <ghaliev@gmail.com>
2025-07-19 10:09:26 +0500

    Испр. ч.1 платежей

--- internal/infrastructure/prodamus_payments/prodamus_payments_handler.go
+++ internal/infrastructure/prodamus_payments/prodamus_payments_handler.go
@@ -1,1 +1,17 @@
 package prodamus_payments
+
+type ProdamusPaymentRequest struct {
+	OrderID     string  `json:"order_id"`
+	Amount      float64 `json:"amount"`
+	Description string  `json:"description"`
+	Email       string  `json:"email"`
+	Recurring   bool    `json:"recurrent"`
+}
+
+type ProdamusPaymentResponse struct {
+	OrderID     string  `json:"order_id"`
+	Amount      float64 `json:"amount"`
+	Description string  `json:"description"`
+	Email       string  `json:"email"`
+	Recurring   bool    `json:"recurrent"`
+}
